074a665926641580ff0882d023e2de7085957c85
Cintra <gui7lk@gmail.com>
2022-11-24 09:49:41 -0300

    feat: header initiazed

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,6 +1,24 @@
+import styled from "styled-components";
+
 export default function Home(){
     return(
         <>
+            <Header>
+                <img src="" alt="Logo"/>
+
+                <div>
+
+                </div>
+            </Header>
         </>
     );
 }
+
+const Header = styled.header`
+    width: 100%;
+    height: 7.5rem;
+    background-color: var(--blue);
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+`;
