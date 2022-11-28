d445dfb9d58e2f5b8f3bae94b5ec9459437a63ab
Pedro Reis <phbarcelosreis66@gmail.com>
2022-11-28 01:35:36 -0300

    feat: add page checkout with some functions

--- src/checkout/checkout.js
+++ src/checkout/checkout.js
@@ -1,16 +1,113 @@
 import styled from "styled-components";
 import axios from "axios";
+import { useEffect, useState } from "react";
 
-function CheckOut(){
+function CheckOut() {
 
+    const [name, setName] = useState("");
+    const [street, setStreet] = useState("");
+    const [hNumber, setHNumber] = useState();
+    const [thing, setThing] = useState("");
+    const [neighborhood, setNeighborhood] = useState("");
+    const [city, setCity] = useState("");
+    const [state, setState] = useState("");
+    const [cep, setCEP] = useState();
 
+    /* --------------------State for Axios--------------------- */
+
+    const [product, setProduct] = useState([])
+
+/*     useEffect(()=> {
+        const promise = axios.get()
+        promise.then((props) => {
+            setProduct(props.data)
+
+        })
+        promise.catch((error) => alert(error))
+    }, []) */
+
+    return (
+        <App>
+            <Product>
+
+            </Product>
+            <Submit onSubmit="">
+                <input type="text" onChange={(e) => setName(e.target.value)} placeholder="Nome Completo" required />
+                <Teste>
+                    <input type="text" onChange={(e) => setStreet(e.target.value)} placeholder="Rua/Logradouro" required />
+                    <input type="text" onChange={(e) => setHNumber(e.target.value)} placeholder="Número da casa/apto" required />
+                </Teste>
+                <Teste>
+                    <input type="text" onChange={(e) => setThing(e.target.value)} placeholder="Complemento" required />
+                    <input type="text" onChange={(e) => setNeighborhood(e.target.value)} placeholder="Bairro" required />
+                </Teste>
+                <Teste>
+                    <input type="text" onChange={(e) => setState(e.target.value)} placeholder="Estado" required />
+                    <input type="text" onChange={(e) => setCity(e.target.value)} placeholder="Cidade" required />
+                </Teste>
+                <input type="text" onChange={(e) => setCEP(e.target.value)} placeholder="CEP" required />
+                <button type="submit"><h1>CONFIRMAR COMPRA</h1></button>
+            </Submit>
+        </App>
+    )
 
 }
 
-const app = styled.div`
+const App = styled.div`
+    box-sizing: border-box;
+    padding-left: 15vw;
+    padding-right: 15vw;
     width: 100vw;
-    height: 100%;
-    
+    height: 100vh;
+    background-color: lightgrey;
+    display: flex;
+    justify-content: center;
+`
+
+const Submit = styled.form`
+    display: flex;
+    margin-top: 100px;
+    width: 100%;
+    flex-direction: column;
+    gap: 10px;
+    align-items: center;
+    & > input{
+        border: none;
+        width: 100%;
+        height: 30px;
+        padding: 10px;
+        border-radius: 5px;
+    } & button {
+        border-radius: 5px;
+        height: 30px;
+        border: none;
+
+    } & h1{
+        font-family: 'Roboto';
+        font-style: normal;
+        font-weight: 700;
+        font-size: 14px;
+        line-height: 16px;
+    }
+
+`
+
+const Teste = styled.div`
+    display: flex;
+    justify-content: center;
+    gap: 5px;
+    width: 100%;
+    & > input{
+        border: none;
+        width: 90%;
+        height: 30px;
+        padding: 10px;
+        border-radius: 5px;
+    }
+
+`
+const Product = styled.div`
+
 
 `
 
